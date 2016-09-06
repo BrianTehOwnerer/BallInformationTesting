45370561d095406189ec15b3e25ef609972496d5
brian <brian@BRIAN-PC>
2016-09-05 19:52:26 -0500

    Add project files.

---
+++ BallInformation/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallInformation
+{
+    class Ball
+    {
+        public int size;
+        public int radius;
+        public bool popped;
+
+
+
+
+    }
+}
---
+++ BallInformation/Color.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallInformation
+{
+    class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public void SetColor(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public void SetColor(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
+
+        public byte GetGreyscale()
+        {
+            byte greyscale = (this.red + this.green + this.blue) / 3;
+            return greyscale;
+        }
+
+
+    }
+}
